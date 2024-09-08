2937b7292bf97fc1c450fbf4b25319d5459ece80
keerthymohan <keerthymohan19@gmail.com>
2024-09-08 21:15:16 +0530

    visitor entry

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,51 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import axios from 'axios'
 
 const ViewAll = () => {
-    const [data,changeData]=useState([])
-    const fetchData=()=>{
-        axios.get("http://35.170.103.9:4057/getvistors").then().catch()
+    const [data, changeData] = useState([])
+    const getDatafromAPI = () => {
+        axios.get("http://35.170.103.9:4057/getvistors").then(
+            (Response) => {
+                changeData(Response.data)
+            }
+        ).catch()
     }
-  return (
-    <div>
-        <Navbar/>
-      <div className="container">
-        <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Purpose</th>
-      <th scope="col">Whom to meet</th>
-      <th scope="col">Date</th>
-    </tr>
-  </thead>
-  <tbody>
+    useEffect(() => { getDatafromAPI() }, [])
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Purpose</th>
+                                            <th scope="col">Whom to meet</th>
+                                            <th scope="col">Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            data.map(
+                                                (value, index) => {
+                                                    return <tr>
+                                                        <th scope="row">{value.firstname}</th>
+                                                        <td>{value.lastname}</td>
+                                                        <td>{value.purpose}</td>
+                                                        <td>{value.whomToMeet}</td>
+                                                        <td>{value.date}</td>
+                                                    </tr>
+                                                }
+                                            )
+                                        }
 
-   {
-    data.map(
-        (value,index)=>{
-            return <tr>
-            <td>{value.firstname}</td>
-            <td>{value.lastname}</td>
-            <td>{value.purpose}</td>
-            <td>{value.whomToMeet}</td>
-            <td>{value.date}</td>
-          </tr>
-        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+        
     )
-   }
-   
-  </tbody>
-</table>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
 }
 
 export default ViewAll
